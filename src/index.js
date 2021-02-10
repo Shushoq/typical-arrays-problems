ceb42ac0deb4cdc9e051e2a55dd72677c46c00e4
shushoq <wywkoff@gmail.com>
2021-02-10 12:25:36 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        array.sort((a, b) => a - b)
+        return array[0]
+    }
+    return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        array.sort((a, b) => a - b)
+        return array[array.length - 1]
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let result = 0
+    let i = 0
+    if (array && array.length > 0) {
+        array.forEach(num => {
+            result += num
+            i++
+        })
+        return result / i
+    }
+    return 0
 }
